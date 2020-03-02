c42abec9439869a1621754fdc4334e228d31bf4a
Siddhant Saoji <sziddhant@gmail.com>
2020-03-02 17:00:36 +0530

    Python controller

---
+++ multi_speed_dynamixel/src/control.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import rospy
+from dynamixel_msgs.msg._MotorVelocityArray import MotorVelocityArray
+def talker():
+    pub = rospy.Publisher('multi_joint_speed_controller/command',  MotorVelocityArray, queue_size=10)
+    rospy.init_node('PythonController', anonymous=True)
+    r = rospy.Rate(10) #10hz
+    
+    while not rospy.is_shutdown():
+        vel=MotorVelocityArray()
+        vel.joint_name=["joint1","joint2"]
+        vel.vel_cmd= [0,0]
+        pub.publish(vel)
+        r.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
