cedc03a268f3522ea298dc355650766e92fb8865
Siddhant Saoji <sziddhant@gmail.com>
2020-03-02 21:07:19 +0530

    Dynamixel Velocity velocity control python script added

--- multi_speed_dynamixel/src/control.py
+++ multi_speed_dynamixel/src/control.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python
+## Dynamixel Velocity control in ROS using python
 
 import rospy
 from dynamixel_msgs.msg._MotorVelocityArray import MotorVelocityArray
-def talker():
+def motorVel(j1,j2,j3,j4,j5,j6):
     pub = rospy.Publisher('multi_joint_speed_controller/command',  MotorVelocityArray, queue_size=10)
     rospy.init_node('PythonController', anonymous=True)
-    r = rospy.Rate(10) #10hz
-    
-    while not rospy.is_shutdown():
-        vel=MotorVelocityArray()
-        vel.joint_name=["joint1","joint2"]
-        vel.vel_cmd= [0,0]
-        pub.publish(vel)
-        r.sleep()
+    #r = rospy.Rate(10) #10hz
+    vel=MotorVelocityArray()
+    vel.joint_name=["joint1","joint3"]
+    vel.vel_cmd= [j1,j2,j3,j4,j5,j6]
+    pub.publish(vel)
+    #print('11')
+    #r.sleep()
 
 if __name__ == '__main__':
     try:
-        talker()
+        
+        motorVel(0,0,0,0,0,0)
+        r = rospy.Rate(5) #10hz
+        r.sleep()
+        motorVel(1,1,1,1,1,1)
+        r.sleep()
+        print('hi')
+        motorVel(10,10,10,10,10,10)
+        r.sleep()
+        motorVel(0,0,0,0,0,0)
     except rospy.ROSInterruptException:
         pass
